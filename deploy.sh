3180be417be3f0cc2a50ee319252834ab2090be8
vbala81 <B.E.VISWANATHAN@ACCENTURE.COM>
2023-11-09 15:07:07 -0500

    Code for notebook instance creation

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+region='us-east-1'
+bucket_name='order-accuracycapture-image-bucket'
+aws s3 cp images/ s3://${bucket_name}/input --recursive
+
+# aws cloudformation create-stack --stack-name might-infra \
+#     --template-body file://cfn/cfn-main.yaml \
+#     --parameters file://cfn/cfn-main.json \
+#     --capabilities CAPABILITY_NAMED_IAM --region ${region}
+
+
+# aws cloudformation update-stack --stack-name might-infra \
+#     --template-body file://cfn/cfn-main.yaml \
+#     --parameters file://cfn/cfn-main.json \
+#     --capabilities CAPABILITY_NAMED_IAM
