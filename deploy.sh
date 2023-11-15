e40840756daa7446bc40c2d783eef323c5156b9e
vbala81 <B.E.VISWANATHAN@ACCENTURE.COM>
2023-11-15 17:54:46 -0500

    Updating notebook

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 region='us-east-1'
 bucket_name='order-accuracycapture-image-bucket'
+# bucket_name='sagemaker-us-east-1-868024899531'
 aws s3 cp images/ s3://${bucket_name}/input --recursive
 
 # aws cloudformation create-stack --stack-name might-infra \
@@ -8,7 +9,7 @@ aws s3 cp images/ s3://${bucket_name}/input --recursive
 #     --capabilities CAPABILITY_NAMED_IAM --region ${region}
 
 
-# aws cloudformation update-stack --stack-name might-infra \
-#     --template-body file://cfn/cfn-main.yaml \
-#     --parameters file://cfn/cfn-main.json \
-#     --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation update-stack --stack-name might-infra \
+    --template-body file://cfn/cfn-main.yaml \
+    --parameters file://cfn/cfn-main.json \
+    --capabilities CAPABILITY_NAMED_IAM
